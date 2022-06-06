57330185f228027963fccb9552e7a28b1a9f82c6
Minenheld <testperson7818@outlook.com>
2022-06-06 22:45:06 +0200

    Add JsonArray, Update JsonObject & Parser

--- src/net/mst/json/JsonObject.java
+++ src/net/mst/json/JsonObject.java
@@ -1,14 +1,15 @@
 package net.mst.json;
 
 import java.util.HashMap;
+import java.util.Set;
 
 public class JsonObject {
 
-	private HashMap<String, Object> data = new HashMap<>();
+	private HashMap<String, Object> data;
 	
 	public JsonObject() {
 		
-		
+		data = new HashMap<>();
 		
 	}
 	
@@ -19,6 +20,34 @@ public class JsonObject {
 			data.put(Key, Value);
 			
 		}
+		
+	}
+	
+	public void removeValue(String Key) {
+		
+		if(data.containsKey(Key)) {
+			
+			data.remove(Key);
+			
+		}
+		
+	}
+	
+	public Boolean contains(String Key) {
+		
+		if(data.containsKey(Key)) {
+			
+			return true;
+			
+		}
+
+		return false;
+		
+	}
+	
+	public Set<String> getKeys() {
+		
+		return data.keySet();
 		
 	}
 	
@@ -34,6 +63,76 @@ public class JsonObject {
 		
 	}
 	
+	public String getString(String Key) {
+		
+		if(data.containsKey(Key)) {
+			
+			return (String) data.get(Key);
+			
+		}
+		
+		return null;
+		
+	}
 	
+	public Integer getInteger(String Key) {
+		
+		if(data.containsKey(Key)) {
+			
+			return (Integer) data.get(Key);
+			
+		}
+		
+		return null;
+		
+	}
+	
+	public Long getLong(String Key) {
+		
+		if(data.containsKey(Key)) {
+			
+			return (Long) data.get(Key);
+			
+		}
+		
+		return null;
+		
+	}
+	
+	public Boolean getBoolean(String Key) {
+		
+		if(data.containsKey(Key)) {
+			
+			return (Boolean) data.get(Key);
+			
+		}
+		
+		return null;
+		
+	}
+	
+	public JsonObject getObject(String Key) {
+		
+		if(data.containsKey(Key)) {
+			
+			return (JsonObject) data.get(Key);
+			
+		}
+		
+		return null;
+		
+	}
+
+	public JsonArray getArray(String Key) {
+	
+	if(data.containsKey(Key)) {
+		
+		return (JsonArray) data.get(Key);
+		
+	}
+	
+	return null;
+	
+}
 	
 }
--- src/net/mst/json/Parser.java
+++ src/net/mst/json/Parser.java
@@ -5,6 +5,8 @@ public class Parser {
 	private String JsonString = null;
 	private JsonObject JsonObject = null;
 	
+	private int current = 0;
+	
 	public Parser() {
 		
 	}
@@ -25,19 +27,400 @@ public class Parser {
 			
 		}
 		
+		return this.JsonObject;
+		
 	}
 	
-	public JsonObject parse(JsonObject JsonObject) {
+	public String parse(JsonObject JsonObject) {
 		
 		this.JsonObject = JsonObject;
+		
+		return getObject(JsonObject);
 		
 	}
 	
 	private JsonObject analyseObject(Integer current) {
 		
 		JsonObject object = new JsonObject();
+		int structure = 0;
 		
+		String name = "";
+		Object tObject;
 		
+		for (int i = current; i < JsonString.length(); i++) {
+			
+			String character = String.valueOf(JsonString.charAt(i));
+			
+			if(character.equals("\"")) {
+				
+				if(!getLast(current).equals("\\")) {
+					
+					if(structure == 0) {
+						
+						structure = 1;
+						
+						continue;
+						
+					}
+					
+					if(structure == 1) {
+						
+						structure = 0;
+						
+						tObject = analyseValue(i, false);
+						
+						object.addValue(name, tObject);
+						
+						i = current;
+						
+						name = "";
+						
+						continue;
+						
+					}
+					
+				}
+				
+			}
+			
+			if(structure == 1) {
+				
+				name = name + character;
+				
+				continue;
+				
+			}
+			
+			if(character.equals("}")) {
+				
+				current = i;
+				
+				return object;
+				
+			}
+			
+		}
+		
+		return object;
+		
+	}
+	
+	private Object analyseValue(Integer cur, Boolean skipReady) {
+		
+		Boolean ready = skipReady;
+		
+		String tString = "";
+		int structure = 0;
+		
+		Integer start = cur;
+		
+		for (int i = cur; i < JsonString.length(); i++) {
+			
+			String character = String.valueOf(JsonString.charAt(i));
+			
+			if(ready) {
+				
+				// Strings
+				
+				if(character.equals("\"")) {
+					
+					if(!getLast(i).equals("\\")) {
+						
+						if(structure == 0) {
+							
+							structure = 1;
+							
+							continue;
+							
+						}
+						
+						if(structure == 1) {
+							
+							current = i;
+							
+							return tString;
+							
+						}
+						
+					}
+					
+				}
+				
+				if(structure == 1) {
+					
+					tString = tString + character;
+					
+					continue;
+					
+				}
+				
+				// Objects and Arrays
+				
+				if(character.equals("{")) {
+					
+					current = i;
+					
+					return analyseObject(current);
+					
+				}
+				
+				if(character.equals("[")) {
+					
+					current = i;
+					
+					return analyseArray(current);
+					
+				}
+				
+				// Other
+				
+				if(character.equals(",") | character.equals("}")) {
+					
+					String value = JsonString.substring(start, i).replace(" ", "");
+					
+					if(value.equals("false")) {
+						
+						current = i - 1;
+						
+						return false;
+						
+					}
+					
+					if(value.equals("true")) {
+						
+						current = i - 1;
+						
+						return true;
+						
+					}
+					
+					if(value.equals("null")) {
+						
+						current = i - 1;
+						
+						return null;
+						
+					}
+					
+					if(isNumber(value)) {
+						
+						current = i - 1;
+						
+						return Integer.parseInt(value);
+						
+					}
+					
+					current = i - 1;
+					
+					return null;
+					
+				}
+				
+			}
+			
+			if(character.equals(":")) {
+				
+				ready = true;
+				
+				start = i + 1;
+				
+				continue;
+				
+			}
+			
+		}
+		
+		return null;
+		
+	}
+	
+	private JsonArray analyseArray(Integer cur) {
+		
+		JsonArray array = new JsonArray();
+		
+		for (int i = cur; i < JsonString.length(); i++) {
+			
+			String character = String.valueOf(JsonString.charAt(i));
+				
+			if(character.equals("[")) {
+				
+				Object object = analyseValue(i + 1, true);
+				
+				if(object == null) {
+					
+					return array;
+					
+				}else {
+					
+					array.addValue(object);
+					
+				}
+				
+				i = current;
+				
+				continue;
+				
+			}
+			
+			if(character.equals(",")) {
+				
+				array.addValue(analyseValue(i + 1, true));
+				
+				i = current;
+				
+				continue;
+				
+			}
+			
+			if(character.equals("]")) {
+				
+				return array;
+				
+			}
+			
+		}
+		
+		return array;
+		
+	}
+	
+	private String getLast(Integer current) {
+		
+		if(current.equals(0)) {
+			
+			return "";
+			
+		}
+		
+		return String.valueOf(JsonString.charAt(current - 1));
+		
+	}
+	
+	private boolean isNumber(String s) {
+		
+		try {
+		
+		    Integer.parseInt(s);
+		    
+		    return true;
+		    
+		} catch (NumberFormatException e) {
+		    
+			return false;
+			
+		}
+		
+	}
+	
+	private String getObject(JsonObject JsonObject) {
+		
+		String main = "{";
+		int i = 0;
+		
+		for(String key : JsonObject.getKeys()) {
+			
+			i++;
+			
+			if(i > 1) {
+				
+				main = main + ", ";
+				
+			}
+			
+			if(JsonObject.get(key) instanceof String) {
+				
+				main = main + "\"" + key + "\":\"" + String.valueOf(JsonObject.get(key)) + "\"";
+				
+				continue;
+				
+			}
+			
+			if(JsonObject.get(key) instanceof Integer) {
+				
+				main = main + "\"" + key + "\":" + (Integer) JsonObject.get(key);
+				
+				continue;
+				
+			}
+			
+			if(JsonObject.get(key) instanceof Boolean) {
+				
+				main = main + "\"" + key + "\":" + (Boolean) JsonObject.get(key);
+				
+				continue;
+				
+			}
+			
+			if(JsonObject.get(key) instanceof JsonObject) {
+				
+				main = main + "\"" + key + "\":" + getObject(JsonObject.getObject(key));
+				
+			}
+			
+			if(JsonObject.get(key) instanceof JsonArray) {
+				
+				main = main + "\"" + key + "\":" + getArray(JsonObject.getArray(key));
+				
+			}
+			
+		}
+		
+		main = main + "}";
+		
+		return main;
+		
+	}
+	
+	private String getArray(JsonArray JsonArray) {
+		
+		String sub = "[";
+		int i = 0;
+		
+		for(Object element : JsonArray.get()) {
+			
+			i++;
+			
+			if(i > 1) {
+				
+				sub = sub + ", ";
+				
+			}
+			
+			if(element instanceof String) {
+				
+				sub = sub + "\"" + String.valueOf(element) + "\"";
+				
+			}
+			
+			if(element instanceof Integer) {
+				
+				sub = sub + (Integer) element;
+				
+			}
+			
+			if(element instanceof Boolean) {
+				
+				sub = sub + (Boolean) element;
+				
+			}
+			
+			if(element instanceof JsonObject) {
+				
+				sub = sub + getObject((JsonObject) element);
+				
+			}
+			
+			if(element instanceof JsonArray) {
+				
+				sub = sub + getArray((JsonArray) element);
+				
+			}
+			
+		}
+		
+		sub = sub + "]";
+		
+		return sub;
 		
 	}
 
